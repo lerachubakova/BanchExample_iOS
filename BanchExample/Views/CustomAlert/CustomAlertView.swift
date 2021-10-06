ff1273d4bd15b8cc54d3dacfc1cad658380ee2f3
Valeriya Chubakova <Valeri160500@gmail.com>
2021-10-06 16:26:15 +0300

    Make CustomAlertController file

--- BanchExample/Views/CustomAlert/CustomAlertView.swift
+++ BanchExample/Views/CustomAlert/CustomAlertView.swift
@@ -20,33 +20,3 @@ class CustomAlertView: UIView {
     }
 
 }
-
-class CustomAlertController: UIViewController {
-    private var alertView: CustomAlertView!
-
-    init(title: String = "Error", text: String = "We have some error", buttontext: String = "OK") {
-        super.init(nibName: nil, bundle: Bundle.main)
-
-        alertView = (Bundle.main.loadNibNamed(String(describing: CustomAlertView.self), owner: self, options: nil)?.first as? CustomAlertView)!
-        alertView.configure(title: title, body: text, buttonTitle: buttontext)
-
-        modalTransitionStyle = .crossDissolve
-        modalPresentationStyle = .overFullScreen
-
-        view = alertView
-    }
-
-    func addAction(title: String) {
-        alertView.button.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
-        alertView.button.setTitle(title, for: .normal)
-        alertView.button.isHidden = false
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    @objc func dismissView() {
-        self.dismiss(animated: true, completion: nil)
-    }
-}
